9e07a95021d6eb4db70e620ffa1a6ef4209c8c05
Adam Bricker <adam.herrala.bricker@gmail.com>
2023-08-14 13:31:49 +0300

    initial BE architecture

---
+++ backend/index.js
@@ -0,0 +1,88 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const Entry = require('./models/entry')
+
+
+//middleware!!
+app.use(express.json())
+app.use(express.static('build')) //for when the static FE is deployed to the BE
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postRes')) //'tiny' formatting plus our custom job
+//add token to morgan for logging POST request only
+morgan.token('postRes', function (req ) {
+    if(req.method === 'POST') {
+        return(
+            JSON.stringify(req.body)
+        )
+    }
+    return(' ')
+})
+
+//dummy data for testing
+
+//Handling requests to server
+
+//get request to load data for all subjects (users won't see this)
+app.get('/api/subjects', (request, response) => {
+    Entry.find({}).then(entries => {response.json(entries)})
+})
+
+//get request for loading data from single subject
+app.get('/api/subjects/:id', (request, response, next) => {
+    Entry.findById(request.params.id)
+    .then(entry => {
+        if (entry) {
+            response.json(entry)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
+})
+
+//post request for creating new (empty) entry --> so users can have unique subject ID for saving later
+app.post('/api/subjects', (request, response) => {
+    const newEntry = new Entry({data : []}) // prevent method from ever being uses to do anything else
+    newEntry.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+
+})
+
+//put request for saving data (using put bc we want it to do the same thing everytime --> replace old w new dataset)
+app.put('/api/subject/:id', (request, response, next) => {
+    const body = request.body
+    const thisID = request.params.id
+    const updates = {data : body.data}
+
+    Entry.findByIdAndUpdate(thisID, updates, {overwrite: true, runValidators: true})
+        .then(() => {response.json(updates)
+        })    
+        .catch(error => next(error))
+})
+
+
+
+//Error handling middleware (needs to go at end!)
+const errorHandler = (error, request, response, next) => {
+    console.error('error:', error.message)
+    console.log(error)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformed id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
+//send out to port
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
---
+++ backend/models/entry.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery',false)
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url)
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+const datumSchema = new mongoose.Schema(
+    {
+        rowNum: {type : String, required : true},
+        condition: {type : String, required: true},
+        stimulus: {type : String, maxLength : 1, required: true, enum : ['1', '0']},
+        response: {type : String, maxLength : 1, required: true, enum : ['1', '0']} 
+    }
+)
+
+const entrySchema = new mongoose.Schema({data: [datumSchema]})
+
+
+entrySchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+
+        returnedObject.data.forEach(i => delete i._id)
+    }
+})
+
+module.exports = mongoose.model('Entry', entrySchema)
